2878f6d98e2977f7ab85fe0cf60c828b76093b8d
daisyarusey <daisyarusey.dc@gmail.com>
2019-07-28 16:09:09 +0300

    Hide additional buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
   $("table").hide();
+  $(".additional-buttons").hide();
+  $(".additional-info").hide();
+  $(".btn.yes").hide();
+  $(".btn.no").hide();
+  $(".additional-info h4").hide();
+
   
 
 });
